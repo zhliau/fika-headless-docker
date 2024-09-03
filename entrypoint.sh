561b4fb66e2a17a62c185304c528959796b6e0af
ZHL <45361542+zhliau@users.noreply.github.com>
2024-09-03 18:43:15 -0400

    Check existence of xvfb before starting client

--- entrypoint.sh
+++ entrypoint.sh
@@ -35,15 +35,15 @@ if [ "$USE_MODSYNC" = "true" ]; then
 fi
 
 if [ "$USE_DGPU" == "true" ]; then
-	source /opt/scripts/install_nvidia_deps.sh
+    source /opt/scripts/install_nvidia_deps.sh
 
-	DISPLAY=:0
-	# Run Xorg server with required extensions
-	sudo /usr/bin/Xorg "${DISPLAY}" vt7 -noreset -novtswitch -sharevts -dpi "${DISPLAY_DPI}" +extension "COMPOSITE" +extension "DAMAGE" +extension "GLX" +extension "RANDR" +extension "RENDER" +extension "MIT-SHM" +extension "XFIXES" +extension "XTEST" &
+    DISPLAY=:0
+    # Run Xorg server with required extensions
+    sudo /usr/bin/Xorg "${DISPLAY}" vt7 -noreset -novtswitch -sharevts -dpi "${DISPLAY_DPI}" +extension "COMPOSITE" +extension "DAMAGE" +extension "GLX" +extension "RANDR" +extension "RENDER" +extension "MIT-SHM" +extension "XFIXES" +extension "XTEST" &
 
-	# Wait for X server to start
-	echo 'Waiting for X Socket' && until [ -S "/tmp/.X11-unix/X${DISPLAY#*:}" ]; do sleep 0.5; done && echo 'X Server is ready'
-	unset XVFB_RUN
+    # Wait for X server to start
+    echo 'Waiting for X Socket' && until [ -S "/tmp/.X11-unix/X${DISPLAY#*:}" ]; do sleep 0.5; done && echo 'X Server is ready'
+    unset XVFB_RUN
 fi
 
 if [ ! -f $EFT_BINARY ]; then
@@ -52,27 +52,32 @@ if [ ! -f $EFT_BINARY ]; then
 fi
 
 run_xvfb() {
-   /usr/bin/Xvfb ":0" -screen 0 1024x768x16 >/dev/null 2>&1 &
+    /usr/bin/Xvfb :0 -screen 0 1024x768x16 >/dev/null 2>&1 &
 }
 
 run_client() {
+    if ! pgrep Xvfb; then
+        echo "Xvfb process not found. Restarting Xvfb."
+        run_xvfb
+    fi
     WINEDEBUG=-all $XVFB_RUN wine $EFT_BINARY $BATCHMODE $NOGRAPHICS $NODYNAMICAI -token="$PROFILE_ID" -config="{'BackendUrl':'http://$SERVER_URL:$SERVER_PORT', 'Version':'live'}"
 }
 
 if [ "$USE_MODSYNC" == "true" ]; then
     while true; do
-	# Anticipate the client exiting due to modsync, and restart it after modsync external updater has done its thing
-	# I don't know why, but it seems on second run of the client it always fails to create a batchmode window,
-	# so we have to restart xvfb after each run
-	echo "Starting Xvfb in background"
-	run_xvfb
-	XVFB_PID=$!
+        pgrep Xvfb && echo "Cleaning up old xvfb processes" && pkill Xvfb
+        # Anticipate the client exiting due to modsync, and restart it after modsync external updater has done its thing
+        # I don't know why, but it seems on second run of the client it always fails to create a batchmode window,
+        # so we have to restart xvfb after each run
+        echo "Starting Xvfb in background"
+        run_xvfb
+        XVFB_PID=$!
 
-	echo "Starting client. Xvfb running PID is $XVFB_PID"
-	run_client
-	echo "Dedi client closed with exit code $?. Restarting.." >&2
-	kill -9 $XVFB_PID
-	sleep 5
+        echo "Starting client. Xvfb running PID is $XVFB_PID"
+        run_client
+        echo "Dedi client closed with exit code $?. Restarting.." >&2
+        kill -9 $XVFB_PID
+        sleep 5
     done
 else
     # run_xvfb
